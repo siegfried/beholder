447dc0cf9c46db232a301e8d1a0df2cbef6304a8
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2025-01-11 03:16:48 +0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A beholder for data";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.rustPlatform.buildRustPackage {
+          pname = "beholder";
+          version = "0.1.0";
+          cargoLock.lockFile = ./Cargo.lock;
+          src = nixpkgs.lib.cleanSource ./.;
+          buildInputs = [ pkgsFor.${system}.postgresql_17 ];
+        };
+      });
+    };
+}
